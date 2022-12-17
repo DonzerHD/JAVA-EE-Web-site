156c83c62355ae3d2af213660b21fd5a51b9f050
DonzerHD <thomas.lemay59@outlook.fr>
2022-12-17 21:34:39 +0100

    init

---
+++ src/main/java/com/test/servlet/Test.java
@@ -0,0 +1,28 @@
+package com.test.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Test extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public Test() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+       
+	 }
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
